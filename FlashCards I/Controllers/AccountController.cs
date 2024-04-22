82d0355f4baf33b9e8c401f7d314c6304e5a6b87
Przem0s84 <przemyslaw.wacek202@gmail.com>
2024-04-22 10:43:26 +0200

    corrections

--- FlashCards I/Controllers/AccountController.cs
+++ FlashCards I/Controllers/AccountController.cs
@@ -1,17 +1,18 @@
 ﻿using FlashCards_I.IServices;
 using FlashCards_I.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlashCards_I.Controllers
 {
     [ApiController]
     [Route("api/account")]
-    public class AccountController: ControllerBase
+    public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
         public AccountController(IAccountService accountService)
         {
-            _accountService= accountService;
+            _accountService = accountService;
         }
 
         [HttpPost("login")]
@@ -37,5 +38,24 @@ namespace FlashCards_I.Controllers
             return Ok();
 
         }
+
+
+        [HttpDelete("delete")]
+        [Authorize]
+        public ActionResult Delete(string email) 
+        {
+            _accountService.DeleteAccount(email);
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Get( ) 
+        {
+            var users = _accountService.GetUsers();
+            if(users==null) return NoContent();
+            return Ok(users);
+        }
+    
     }
 }
--- FlashCards I/IServices/IAccountService.cs
+++ FlashCards I/IServices/IAccountService.cs
@@ -4,8 +4,10 @@ namespace FlashCards_I.IServices
 {
     public interface IAccountService
     {
+        void DeleteAccount(string email);
         string GenerateToken(LoginUserDto loginUserDto);
         void RegisterUser(RegistrationUDto regdto);
         void ResetPassword(ResetPasswordDto resetdto);
+        List<ReturnUsersDto> GetUsers();
     }
 }
